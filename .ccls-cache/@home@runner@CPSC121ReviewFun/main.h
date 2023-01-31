334f4a37540d27d9dd4c86228a3f3f3d126b431c
milesmercer04 <milesmercer04@gmail.com>
2023-01-31 20:38:59 +0000

    add comment

---
+++ .ccls-cache/@home@runner@CPSC121ReviewFun/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+
+#include <iostream>
+#include <vector>
+
+void fillVector(std::vector<int>&);
+void findMinMax(std::vector<int>&, int &, int &);
+
+#endif
---
+++ .ccls-cache/@home@runner@CPSC121ReviewFun/myfuncs.h
@@ -0,0 +1,12 @@
+#ifndef MYFUNCS_H
+#define MYFUNCS_H
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void fillVector(vector<int>&);
+void findMinMax(vector<int>&, int &, int &);
+
+#endif
